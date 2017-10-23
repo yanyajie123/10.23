2d8f0deb9b2a2eba0b7a49088cf0bd3f49a5f4c0
yanyajie <1678212014@qq.com>
2017-10-23 21:02:54 +0800

    10.23
    10.23  zuoye

---
+++ yyj/public/javascripts/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2017/10/23.
+ */
+//$(function(){
+//    $('.header').click(function(){
+//        alert(1)
+//    })
+//
+//})
+var set=document.getElementsByClassName('set')
+console.log(set)
